a0dbe5bc2e8d0b55d18a60b8a967961b5a8b9e25
backdownof <bogdanovan88@gmail.com>
2024-03-13 12:15:18 +0500

    added next word and start handlers

---
+++ views/helpers/keyboard.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from aiogram import types
+from aiogram.utils.keyboard import InlineKeyboardBuilder, ReplyKeyboardBuilder
+
+
+def build_inline_keyboard(buttons: List[tuple], adjust_num: int = None):
+    ikb = InlineKeyboardBuilder()
+
+    for button_text, callback in buttons:
+        ikb.add(types.InlineKeyboardButton(
+            text=button_text,
+            callback_data=callback
+        ))
+
+    if adjust_num:
+        ikb.adjust(adjust_num)
+
+    return ikb.as_markup()
---
+++ views/words.py
@@ -0,0 +1,54 @@
+import transaction
+
+from db import models
+from views.constants.buttons import Button
+from views.constants.callbacks import Callback
+from views.constants.messages import Message
+from views.helpers import messages as message_helpers, keyboard as kb_helpers
+
+from aiogram import types, Router, F
+from sqlalchemy import tablesample, func
+from sqlalchemy.orm import aliased
+
+router = Router()
+
+
+@router.callback_query(F.data == Callback.NEXT_WORD)
+async def new_word(event: types.Message):
+    user = models.User.get_by_tg_id(event.from_user.id)
+    if not user:
+        user = models.User(
+            full_name=event.from_user.full_name,
+            tg_id=event.from_user.id,
+            tg_nickname=event.from_user.username,
+        )
+        user.add()
+
+        transaction.commit()
+        user.add()
+
+    keyboard_data = [
+        (Button.NEXT_WORD, Callback.NEXT_WORD),
+        (Button.SETTINGS, Callback.SETTINGS),
+    ]
+    kb = kb_helpers.build_inline_keyboard(keyboard_data, adjust_num=2)
+
+    random_word_alias = aliased(
+        models.Word,
+        tablesample(models.Word, func.bernoulli(2.5))
+    )
+    word_and_translation: models.WordExamples = models.DBSession.query(
+        random_word_alias,
+        models.WordTranslation
+    ).join(
+        models.WordTranslation,
+        models.WordTranslation.word_id == random_word_alias.id,
+    ).limit(1).first()
+
+    if not word_and_translation:
+        await message_helpers.send_message(event, Message.NO_NEW_WORDS, reply_markup=kb)
+        return
+
+    message = message_helpers.MessageTemplates.get_new_word_message(word_and_translation)
+
+    await message_helpers.send_message(event, message, reply_markup=kb)
